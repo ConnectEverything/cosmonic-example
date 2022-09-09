a80ddfe7166fb35f16e85500df4c72c1d534dbe0
Derek Collison <derek@nats.io>
2022-09-09 15:09:08 -0700

    Added broadcast example as well
    Signed-off-by: Derek Collison <derek@nats.io>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/nats-io/nats-server/v2/server"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nuid"
 
 	natsserver "github.com/nats-io/nats-server/v2/test"
 )
@@ -29,14 +30,36 @@ func main() {
 	c1.Subscribe("cosmonic.requests", reqHandlerCust1)
 	c2.Subscribe("cosmonic.requests", reqHandlerCust2)
 
+	// Also for broadcast requests.
+	c1.Subscribe("cosmonic.all", reqHandlerCust1)
+	c2.Subscribe("cosmonic.all", reqHandlerCust2)
+
 	// Create our root user who will send the requests.
 	nc := natsConnect("root", "s3cr3t!", nats.CustomInboxPrefix("$R"))
 	defer nc.Close()
+
+	log.Printf("Sending customer specific requests")
 
 	// Make a system request to cust1
 	sysRequest(nc, "cust1")
 	// Make a system request to cust2
 	sysRequest(nc, "cust2")
+
+	// Now send a broadcast request and collect responses.
+
+	inbox := fmt.Sprintf("$R.%s", nuid.Next())
+	sub, _ := nc.SubscribeSync(inbox)
+
+	log.Printf("Sending broadcast request")
+	nc.PublishRequest("broadcast", inbox, nil)
+
+	for {
+		r, err := sub.NextMsg(time.Second)
+		if err != nil {
+			break
+		}
+		log.Printf("Got a broadcast response: %q", r.Data)
+	}
 }
 
 // This sends a request and gets a response.
@@ -46,7 +69,7 @@ func sysRequest(nc *nats.Conn, cust string) {
 	if err != nil {
 		log.Fatalf("Error sending request: %v")
 	}
-	log.Printf("Got a response for %q: %q", cust, r.Data)
+	log.Printf("Got a direct response for %q: %q", cust, r.Data)
 }
 
 // Callback for customers to receive requests.
